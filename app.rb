7ceffcdb493ff570875b09ff8e97955410d05a09
aciukurescu <andrewciukurescu@gmail.com>
2018-02-09 14:13:23 -0500

    added link to send email to us

--- app.rb
+++ app.rb
@@ -16,8 +16,24 @@ get '/poker' do
 	erb :poker
 end
 
-get '/contact' do 
-	erb :contact
+get '/contact' do
+@email = params[:email]
+@subject = params[:subject]
+@content = params[:content]
+
+from = Email.new(email: @email)
+to = Email.new(email: 'andrewciukurescu@gmail.com')
+subject = @subject
+content = Content.new(type: 'text/plain', value: @content)
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+
+erb :thanks
 end
 
 get '/blackjack' do
