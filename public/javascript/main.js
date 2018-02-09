7a30fd6f663a11fa6d93a573acff13e3302761d7
Karen Mitchell <karen1752000@yahoo.com>
2018-02-09 15:56:00 -0500

    navbar animated

---
+++ public/javascript/main.js
@@ -0,0 +1,11 @@
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+bars.addEventListener("click", barClicked, false);
+
+
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
