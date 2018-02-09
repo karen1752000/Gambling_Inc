2368170d6ff20f90e9211b12981e8b93722968ef
aciukurescu <andrewciukurescu@gmail.com>
2018-02-09 15:05:49 -0500

    when submitting an email you now get linked to a page thanking you for signing up

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do    
     erb :index
@@ -17,6 +19,10 @@ get '/poker' do
 end
 
 get '/contact' do
+	erb :contact
+end
+
+get '/thanks' do
 @email = params[:email]
 @subject = params[:subject]
 @content = params[:content]
@@ -26,15 +32,14 @@ to = Email.new(email: 'andrewciukurescu@gmail.com')
 subject = @subject
 content = Content.new(type: 'text/plain', value: @content)
 mail = Mail.new(from, subject, to, content)
-
 sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
 puts response.headers
-
-erb :thanks
+	erb :thanks
 end
+
 
 get '/blackjack' do
 	erb :blackjack
