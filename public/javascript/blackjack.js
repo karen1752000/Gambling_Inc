b65417513381a06ac6472b921dfb8d8da7c53fab
aciukurescu <andrewciukurescu@gmail.com>
2018-02-11 15:29:55 -0500

    second scenario game added

--- public/javascript/blackjack.js
+++ public/javascript/blackjack.js
@@ -1,12 +1,23 @@
 var scenario = document.getElementById('scenario');
 var hit1 = document.getElementById('hit1');
 var stay1 = document.getElementById('stay1');
+var hit2 = document.getElementById('hit2');
+var stay2 = document.getElementById('stay2');
 
 
 hit1.addEventListener('click', function(){
-	alert("correct")
+	alert("Correct!")
 });
 
 stay1.addEventListener('click', function(){
 	alert("When the dealer is showing a 7 or higher, and you have a 16 you should hit!")
 })
+
+hit2.addEventListener('click', function(){
+	alert("Whatever the dealer is showing, and you have a 17 or higher you should stand!")
+});
+
+stay2.addEventListener('click', function(){
+	alert("Correct!")
+})
+
