291ed7d47c0054bc98ca6a0a2b270927d1b7dcd6
aciukurescu <andrewciukurescu@gmail.com>
2018-02-11 15:35:56 -0500

    third scneario added

--- public/javascript/blackjack.js
+++ public/javascript/blackjack.js
@@ -3,6 +3,8 @@ var hit1 = document.getElementById('hit1');
 var stay1 = document.getElementById('stay1');
 var hit2 = document.getElementById('hit2');
 var stay2 = document.getElementById('stay2');
+var hit3 = document.getElementById('hit2');
+var stay3 = document.getElementById('stay2');
 
 
 hit1.addEventListener('click', function(){
@@ -21,3 +23,10 @@ stay2.addEventListener('click', function(){
 	alert("Correct!")
 })
 
+hit3.addEventListener('click', function(){
+	alert("When the dealer is showing either a 4, 5, or 6 and you have a 12 you should stand!")
+});
+
+stay3.addEventListener('click', function(){
+	alert("Correct!")
+})
