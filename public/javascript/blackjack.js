c7f7bb31ceb4fda898bc884d2704e5e783690cd8
aciukurescu <andrewciukurescu@gmail.com>
2018-02-11 15:24:47 -0500

    made basic js that shows what to do in hand scenario

--- public/javascript/blackjack.js
+++ public/javascript/blackjack.js
@@ -1,1 +1,12 @@
+var scenario = document.getElementById('scenario');
+var hit1 = document.getElementById('hit1');
+var stay1 = document.getElementById('stay1');
 
+
+hit1.addEventListener('click', function(){
+	alert("correct")
+});
+
+stay1.addEventListener('click', function(){
+	alert("When the dealer is showing a 7 or higher, and you have a 16 you should hit!")
+})
